56c57e431754eae4995d165e86712aaa7286c760
github username <github email address>
2019-12-16 15:00:28 -0600

    mod 3 code challenge

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 4141 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +9,71 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  function getImage(){
+    fetch(imageURL)
+    .then(response => response.json())
+    .then(imageData => displayPage(imageData))
+  }
+
+  getImage()
+
+  function displayPage(imageData) {
+    document.querySelector('#image').src = imageData.url
+    document.querySelector('#name').innerText = imageData.name
+    document.querySelector('#likes').innerText = imageData.like_count
+
+    comments = imageData.comments
+
+    comments.forEach(comment => {
+      document.querySelector('#comments').innerHTML += `
+      <li>${comment.content}</li>`
+    });
+
+    console.log(imageData.id)
+    likeButton = document.getElementById('like_button')
+    
+    likeButton.addEventListener('click', function() {
+     numLikes = parseInt(document.getElementById('likes').innerText)
+     document.querySelector('#likes').innerText = numLikes += 1
+     let total = numLikes += 1
+     
+     let data = {
+      image_id: imageData.id,
+      like_count: total
+     }
+
+     fetch(likeURL, {
+       method: 'POST',
+       headers: {
+         'Content-Type': 'application/json',
+         'Accept': 'application/json'
+       },
+         body: JSON.stringify(data)
+     })
+    })
+
+    let submitComment = document.getElementById('comment_form')
+    
+    submitComment.addEventListener('submit', function(e){
+      e.preventDefault()
+      let newComment = document.getElementById('comment_input').value
+      document.querySelector('#comments').innerHTML += `
+      <li>${newComment}</li>`
+
+      let data = {
+        image_id: imageData.id,
+        content: newComment
+       }
+  
+       fetch(commentsURL, {
+         method: 'POST',
+         headers: {
+           'Content-Type': 'application/json',
+           'Accept': 'application/json'
+         },
+           body: JSON.stringify(data)
+       })
+    })
+
+  }
 })
